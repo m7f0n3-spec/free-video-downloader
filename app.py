d7d3e3af091b6f871de3ba5794506f6e04aa76d1
root <root@kali.mylinux.com>
2026-08-22 10:19:21 +0300

    quick update

--- app.py
+++ app.py
@@ -143,12 +143,12 @@ def download_video():
         'geo_bypass': True,
         'extractor_args': {
             'youtube': {
-                'player_client': ['android', 'web'],
+                'player_client': ['mweb', 'android', 'ios'],
                 'skip': ['hls', 'dash']
             }
         },
         'headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
         }
     }
 
