1e717f69e123fc99d7304cae6bb0428d13168091
root <root@kali.mylinux.com>
2026-09-07 20:36:06 +0300

    quick update

--- app.py
+++ app.py
@@ -417,6 +417,6 @@ def download_video():
       del progress_queues[task_id]
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
   port = int(os.environ.get('PORT', 8080))
   app.run(host='0.0.0.0', port=port, debug=False)
